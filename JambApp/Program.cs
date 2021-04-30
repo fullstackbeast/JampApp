c7bf440cf1cea4ab619f09adb533d112982120ee
Omolola Toyyib Babatunde <dpoohwinnie0@gmail.com>
2021-04-30 12:01:38 +0100

    Finished Registration Class

---
+++ JambApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace JambApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Registration registration = new Registration();
+
+            bool continueReg = true;
+
+            while (continueReg)
+            {
+                Aspirant aspirant = registration.Register();
+                
+                Console.WriteLine(aspirant.RegistrationNumber);
+                
+                Console.Write("Do you want to continue(y/n): ");
+                string answer = Console.ReadLine().ToLower().Trim();
+
+                if (answer == "n") continueReg = false;
+
+            }
+        }
+    }
+}
